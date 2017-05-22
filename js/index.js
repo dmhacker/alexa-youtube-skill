13073a10b98310872ca6486f21fa115c3417421b
dmhacker <dmhacker@yahoo.com>
2017-05-22 07:40:46 -0700

    Minor changes

--- js/index.js
+++ js/index.js
@@ -34,38 +34,43 @@ app.intent("GetVideoIntent", {
     function(req, response) {
         var query = req.slot("VideoQuery");
 
+        console.log('Searching ... ' + query);
+
         search(query, searchOpts, function(err, results) {
             if (err) {
                 response.fail(err.message);
-            }
-            else if (results.length !== 1) {
+            } else if (results.length !== 1) {
                 response.say('I could not complete your request at this moment.').send();
             } else {
                 var metadata = results[0];
                 if (metadata.id === undefined) {
                     response.say(query + ' did not return any results on YouTube.').send();
                 } else {
-                    response.say('I found a relevant video called '+metadata.title+'.');
+                    response.say('I found a relevant video called ' + metadata.title + '.');
 
-                    var externalDownload = 'https://dmhacker-youtube.herokuapp.com/alexa/'+metadata.id;
-                    request(externalDownload, function (err, res, body) {
+                    console.log('Found ... ' + metadata.title);
+
+                    var externalDownload = 'https://dmhacker-youtube.herokuapp.com/alexa/' + metadata.id;
+                    request(externalDownload, function(err, res, body) {
+                        console.log('Processed.');
+
                         if (err) {
                             console.log(err);
                             console.log(body);
                             response.fail(err.message);
-                        }
-                        else {
+                        } else {
                             lastSearch = JSON.parse(body).link;
-                            response.audioPlayerPlayStream('REPLACE_ALL', {
+                            var stream = {
                                 'url': lastSearch,
-                                'streamFormat': 'AUDIO_MPEG',
                                 'token': metadata.id,
                                 'offsetInMilliseconds': 0
-                            });
+                            };
+                            console.dir(stream);
+                            response.audioPlayerPlayStream('REPLACE_ALL', stream);
                             response.card({
                                 'type': 'Simple',
-                                'title': 'Search for "'+query+'"',
-                                'content': 'Alexa found "'+metadata.title+'" at '+metadata.link + '.'
+                                'title': 'Search for "' + query + '"',
+                                'content': 'Alexa found "' + metadata.title + '" at ' + metadata.link + '.'
                             });
                             response.send();
                         }
@@ -77,6 +82,11 @@ app.intent("GetVideoIntent", {
         return false;
     }
 );
+
+app.audioPlayer("PlaybackStarted", function(request, response) {
+    console.log('Now playing audio clip ...');
+    console.dir(request);
+});
 
 app.intent("AMAZON.PauseIntent", {}, function(req, response) {
     response.audioPlayerStop();
