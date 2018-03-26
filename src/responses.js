a40cc84f807182c9d34805bee0a7eb7abf11afb2
David Hacker <dmhacker.cs@gmail.com>
2018-03-26 11:46:22 -0700

    Edit help response so it is more clear

--- src/responses.js
+++ src/responses.js
@@ -7,8 +7,7 @@ module.exports = {
     "LOOP_ON_TRIGGERED": "I will repeat your {0} selection infinitely.",
     "LOOP_OFF_TRIGGERED": "I will no longer repeat your {0} selection.",
     "REPEAT_TRIGGERED": "I will repeat your {0} selection once.",
-    "HELP_TRIGGERED": "To use the YouTube skill, tell the skill to search for whatever video you want." +
-                      "You can use pause, resume, repeat, start over, loop on, and loop off directives as well."
+    "HELP_TRIGGERED": "To use the YouTube skill, tell the skill to search for the video you want. Additionally, once the video is playing, you can tell Alexa to  pause, restart, or loop it."
   },
   "de-DE": {
     "NO_RESULTS_FOUND": "Keine Ergebnisse auf Youtube gefunden.",
@@ -18,7 +17,6 @@ module.exports = {
     "LOOP_ON_TRIGGERED": "Ich werde deine Auswahl unendlich wiederholen.",
     "LOOP_OFF_TRIGGERED": "Ich werde deine Auswahl nicht mehr wiederholen.",
     "REPEAT_TRIGGERED": "Ich werde deine Auswahl einmal wiederholen.",
-    "HELP_TRIGGERED": "Um die YouTube-Fertigkeit zu verwenden, musst du die Fähigkeit angeben, nach dem gewünschten Video zu suchen." +
-                      "Sie können auch die Anweisungen pause, resume, repeat, neu starten, loop on und loop verwenden."
+    "HELP_TRIGGERED": "Um die YouTube-Fertigkeit zu verwenden, musst du die Fähigkeit angeben, nach dem gewünschten Video zu suchen. Wenn das Video abgespielt wird, können Sie Alexa außerdem mitteilen, dass es angehalten, neu gestartet oder wiederholt werden soll."
   }
 }
