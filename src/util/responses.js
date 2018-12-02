6f277a31d8a4a1009d35b2c4da72897ff777adc0
David Hacker <dmhacker@protonmail.com>
2018-12-02 12:48:58 -0800

    Move utility files to separate folder

---
+++ src/util/responses.js
@@ -0,0 +1,46 @@
+module.exports = {
+  "en-US": {
+    "NO_RESULTS_FOUND": "{0} did not return any results on YouTube.",
+    "ASK_TO_PLAY": "I found a video called {0}. Would you like me to play it?",
+    "NOW_PLAYING": "I am now playing {0}.",
+    "NOTHING_TO_RESUME": "You are not playing anything currently.",
+    "NOTHING_TO_REPEAT": "You have not selected a video to play.",
+    "LOOP_ON_TRIGGERED": "I will repeat your {0} selection infinitely.",
+    "LOOP_OFF_TRIGGERED": "I will no longer repeat your {0} selection.",
+    "REPEAT_TRIGGERED": "I will repeat your {0} selection once.",
+    "HELP_TRIGGERED": "To use the YouTube skill, tell the skill to search for the video you want. Additionally, once the video is playing, you can tell Alexa to  pause, restart, or loop it."
+  },
+  "de-DE": {
+    "NO_RESULTS_FOUND": "Keine Ergebnisse auf Youtube gefunden.",
+    "ASK_TO_PLAY": "Ich habe ein Video mit dem Namen {0} gefunden. Möchten Sie, dass ich es spiele?",
+    "NOW_PLAYING": "Ich spiele jetzt {0}.",
+    "NOTHING_TO_RESUME": "Sie spielen derzeit nichts.",
+    "NOTHING_TO_REPEAT": "Sie haben kein Video zum Abspielen ausgewählt.",
+    "LOOP_ON_TRIGGERED": "Ich werde deine Auswahl unendlich wiederholen.",
+    "LOOP_OFF_TRIGGERED": "Ich werde deine Auswahl nicht mehr wiederholen.",
+    "REPEAT_TRIGGERED": "Ich werde deine Auswahl einmal wiederholen.",
+    "HELP_TRIGGERED": "Um die YouTube-Fertigkeit zu verwenden, musst du die Fähigkeit angeben, nach dem gewünschten Video zu suchen. Wenn das Video abgespielt wird, können Sie Alexa außerdem mitteilen, dass es angehalten, neu gestartet oder wiederholt werden soll."
+  },
+  "fr-FR": {
+    "NO_RESULTS_FOUND": "{0} n'a pas été trouvé sur YouTube.",
+    "ASK_TO_PLAY": "J'ai trouvé une vidéo appelée {0}. Voulez-vous que je joue?",
+    "NOW_PLAYING": "Je suis en train de jouer {0}.",
+    "NOTHING_TO_RESUME": "Il n'y a aucun élément en cours de lecture.",
+    "NOTHING_TO_REPEAT": "Vous n'avez pas sélectionné de vidéo à écouter.",
+    "LOOP_ON_TRIGGERED": "Je vais répéter {0} à l'infini.",
+    "LOOP_OFF_TRIGGERED": "Je ne vais plus répéter {0} .",
+    "REPEAT_TRIGGERED": "Je ne vais répéter {0} une fois.",
+    "HELP_TRIGGERED": "Pour utiliser cette skill, dites à Alexa de rechercher la vidéo que vous souhaitez. De plus, lorsque la lecture est en cours, vous pouvez dire à Alexa de mettre en pause, redémarrer, ou répéter."
+  },
+  "it-IT": {
+    "NO_RESULTS_FOUND": "{0} non lo trovo su YouTube.",
+    "ASK_TO_PLAY": "Ho trovato un video chiamato {0}. Vorresti che lo suonassi?",
+    "NOW_PLAYING": "Sto suonando {0}.",
+    "NOTHING_TO_RESUME": "Non stai suonando nulla ora.",
+    "NOTHING_TO_REPEAT": "Non hai selezionato nessun video da riprodurre.",
+    "LOOP_ON_TRIGGERED": "Ti ripeterò la tua selezione su {0} infinitamente.",
+    "LOOP_OFF_TRIGGERED": "Non ti ripeterò più la tua selezione su {0}.",
+    "REPEAT_TRIGGERED": "Ti suonerò la tua selezione su {0} solo una volta.",
+    "HELP_TRIGGERED": "Per usare la skill di YouTube, chiedi alla skill di cercare il video che vuoi. In aggiunta, una volta che il video è in riproduzione, puoi chiedere ad Alexa di metterlo in pausa, riavviarlo, o di metterlo in loop."
+  }
+}
